2c08df8758a92c929248b23f59becb541eb56289
智智-GitHub <101373626+Little-wisdom2022@users.noreply.github.com>
2022-08-25 22:35:02 +0800

    Update and rename home.cmd to 1.0 版/home.cmd

---
+++ 1.0 版/home.cmd
@@ -0,0 +1,16 @@
+cls
+@echo off
+:: COPYRIGHT (C) 2022 ZHIZHI. SOME RIGHTS RESERVED.
+:: 智智 保留部分权利。
+:: 以转载、二次编译等形式进行传播时请注明原作者。
+
+title LW KMS Office激活工具 BY 智智
+echo LW KMS Office激活工具 BY 智智
+echo  按任意键开始激活
+pause>nul
+slmgr /skms kms.03k.org
+slmgr /ato
+cls
+start HEU.exe
+echo 最后一步！在新弹出的窗口点击确认后，进入“其他”页面选择“Fix_Banner_New”，然后点击“开始”即可完成修复。 
+cmd/k
